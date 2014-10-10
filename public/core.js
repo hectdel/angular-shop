c9d1316b85be4f30035356c3265db22b94c4140d
hectdel <hector.delmel@gmail.com>
2014-10-10 10:19:15 +0200

    Filtro de busqueda de tablas

--- public/core.js
+++ public/core.js
@@ -3,10 +3,15 @@ var shop = angular.module('shop', []);
 function cuentasCtrl($scope, $http) {
     $scope.formData = {};
     $scope.cuentasList = [];
+
+    $scope.searchFilter = function (cuenta) {
+        var keyword = new RegExp($scope.nameFilter, 'i');
+        return !$scope.nameFilter || keyword.test(cuenta.name) || keyword.test(cuenta.email);
+    };
+
     // when landing on the page, get all todos and show them
     $http.get('/cuentas')
         .success(function (data) {
-            console.log('entra ');
             $scope.cuentasList = data;
             console.log(data);
         })
@@ -14,6 +19,12 @@ function cuentasCtrl($scope, $http) {
             console.log('Error: ' + data);
         });
 
+
+
+
 }
+
+
+
 
 
